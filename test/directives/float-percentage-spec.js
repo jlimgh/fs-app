01b5b2fca99a1c887e0f9007ada81867e90f495c
Jefferson Lim <jefferer@gmail.com>
2017-07-19 03:44:35 +0000

    [feature] cleaned up html, directive, and testing files

--- test/directives/float-percentage-spec.js
+++ test/directives/float-percentage-spec.js
@@ -3,8 +3,22 @@ describe("floatPercentage", function() {
   	  $scope,
   	  $compile,
   	  $el,
+  	  elem,
   	  $body = $('body'),
-  	  simpleHtml = "<form name='form'>" + "<input ng-model='length' float-percentage='float'></input>" + "</form>";
+  	  simpleHtml = '<form name="form" novalidate>' +
+                      '<div class="form-group">' +
+                        '<label for="usr">Input:</label>' +
+                         '<input type="text" id="float" class="form-control" ng-model="length" name="length" float-percentage="float" />' +
+                          '<strong> {{length}} </strong>' +
+                          '<br />' +
+                          '<span class="error" ng-show="form.length.$error.float">' + 
+                            'This is not a valid float number!' +
+                          '</span>' +
+                          '<span class="error" ng-show="form.length.$error.number">' +
+                              'Range must be between 0 and 100!' +
+                          '</span>' +
+                        '</div>' +
+                      '</form>';
 
   beforeEach(function() {
   	module('app');
@@ -13,40 +27,43 @@ describe("floatPercentage", function() {
   		$rootScope = $injector.get('$rootScope');
   		$scope = $rootScope.$new();
   		$compile = $injector.get('$compile');
-  		el = $compile(angular.element(simpleHtml))($scope);
+  	  elem = angular.element(simpleHtml);
+  		$compile(elem)($scope);
   	});
 
-  	$body.append(el);
-  	$rootScope.$digest();
-  });
-
-  it("should exist", function() {
-  	expect(el.hasClass("float")).toBeFalsy();
   	$scope.$digest();
-  	console.log("el", el);
-  	expect(el.hasClass("float")).toBeTruthy();
   });
 
 
 
-  it("Should show an notification error if it is not a float number", function () {
-    // $compile(dirElement)($scope);
-    // $scope.$digest();
+  it("should exist on the DOM and as a input attribute", function() {
+    
+  	expect(elem.find("input").attr("float-percentage")).toBeTruthy();
+  	$scope.$digest();
+  	
+  });
 
-    // Find the input control: 
-    console.log("el: ", el);
-    console.log("floatpercentage", el.attr("float-percentage"));
-    var dirElementInput = el.find('input');
-
-    // Set some text!
-    angular.element(dirElementInput).val('Some text').trigger('input');
+  it("should take in a model and make it a float number", function() {
+    
+    elem.val("5");
     $scope.$apply();
+    $scope.$digest();
+    
+  	expect(elem.val()).toEqual('5');
+  	
+  });
 
-    console.log("dirElementInput", dirElementInput);
-    // Check the outcome is what you expect! (in my case, that a specific class has been applied)
-    expect(dirElementInput.hasClass('ng-valid')).toEqual(true);
+
+  it("should show notification error if input is not a float number", function() {
+    
+    expect(elem.attr("span")).toBeFalsy();
+    elem.val("f").trigger("input");
+    $scope.$apply();
+    $scope.$digest();
+
+  	expect(elem.find("span").hasClass("error")).toEqual(true);
+  	
   });
 
 
 });
-
